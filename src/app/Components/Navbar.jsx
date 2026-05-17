86e1f1d0eeeac165792dc15e0bb3d1b872d32473
Symun Hossain Shifat <saymonshifat569625@gmail.com>
2026-05-17 23:57:23 +0600

    navbar and footer added

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,8 +1,52 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import logo from '@/asset/logo.png'
 
 function Navigationpage () {
   return (
-    <div>Navigationpage </div>
+    <div>
+        <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu font-semibold menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/'}>All Appointment</Link></li>
+        <li><Link href={'/'}>Dashboard</Link></li>
+        
+      </ul>
+    </div>
+   <Image src={logo} alt="Logo image" className='border' width={90} height={90}/>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu font-semibold  menu-horizontal px-1">
+      <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/'}>All Appointment</Link></li>
+        <li><Link href={'/'}>Dashboard</Link></li>
+        
+    </ul>
+  </div>
+  <div className="navbar-end flex gap-3">
+    <Link href={'/'}>
+    <button className='btn btn-neutral btn-outline'>
+    Login
+   </button>
+    </Link>
+    <Link href={'/'}>
+    <button className='btn btn-neutral btn-outline'>
+    Register
+   </button>
+    </Link>
+   
+   
+  </div>
+</div>
+    </div>
   )
 }
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navigationpage from "./Components/Navbar";
+import Footerpage from "./Components/footer";
 
 
 const geistSans = Geist({
@@ -21,13 +23,15 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      data-theme = 'light'
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        
+       <Navigationpage></Navigationpage>
         
         {children}
         
+        <Footerpage></Footerpage>
         
         </body>
     </html>
